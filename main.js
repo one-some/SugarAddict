bde43e8bccc4e0482759958e0542a4586a4ecd69
onesome <onesomegit@outlook.com>
2023-02-11 11:57:23 -0600

    Variable finder

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ browser.runtime.onMessage.addListener(function (data) {
     if (data === "init") tryInit();
 });
 
+function getWindowVariables() {
+    // Genius solution by jungy
+    // https://stackoverflow.com/a/17246535
+
+    // Make a clean slate window--no custom properties
+    let iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    document.body.appendChild(iframe);
+
+    // Here's our (tainted) property list
+    let currentWindow = Object.getOwnPropertyNames(window.wrappedJSObject);
+
+    // Filter based on presence in clean slate
+    let results = currentWindow.filter(function(prop) {
+        return !iframe.contentWindow.hasOwnProperty(prop);
+    });
+
+    document.body.removeChild(iframe);
+
+    return results;
+}
+
 async function tryInit() {
     // Profile current game engine. If no supported game engine is found, go home.
     if (injected) return;
@@ -20,6 +42,8 @@ async function tryInit() {
         const { initRenPyWeb } = await import(browser.runtime.getURL("engines/renpy.js"));
         await initRenPyWeb();
     } else {
-        console.error("Nothing :^(");
+        console.error("Nothing :^( Dumping window variables...");
+        let vars = getWindowVariables();
+        console.log(vars);
     }
 }
