f2676027355c508cd7bad07a59dec675954370b3
somebody <onesomegit@outlook.com>
2023-06-14 12:30:09 -0500

    Window resizing

--- ui/window.js
+++ ui/window.js
@@ -1,8 +1,19 @@
-let isMinimized = false;
-let dragOffset = null;
-let dragEl = null;
+const WINDOW_MIN_WIDTH = 250;
+const WINDOW_MIN_HEIGHT = 300;
+
+const DragState = {
+    WINDOW_POSITION: 0,
+    RESIZE_BOTTOM_LEFT: 1,
+    RESIZE_BOTTOM_RIGHT: 2
+};
 
 let windowContainer;
+let dragState = null;
+let windowResizeRectBuffer = null;
+let dragOffset = null;
+let dragEl = null;
+let isMinimized = false;
+
 const decache = Math.random();
 
 document.addEventListener("keydown", function (event) {
@@ -39,7 +50,9 @@ export async function makeWindow(tabs) {
     windowContainer.addEventListener("click", dontPropagate);
     windowContainer.addEventListener("wheel", dontPropagate);
 
-    const topBar = $e("div", windowContainer, { id: "sa-topbar" });
+    const windowContents = $e("div", windowContainer, { id: "sa-window-contents" });
+
+    const topBar = $e("div", windowContents, { id: "sa-topbar" });
 
     // Hack
     $e("spacer", topBar);
@@ -50,7 +63,7 @@ export async function makeWindow(tabs) {
         id: "sa-minimize",
     });
 
-    const main = $e("div", windowContainer, { id: "sa-main" });
+    const main = $e("div", windowContents, { id: "sa-main" });
     const tabBar = $e("div", main, { id: "sa-tabbar" });
 
     const tabContentContainer = $e("div", main, {
@@ -58,6 +71,8 @@ export async function makeWindow(tabs) {
     });
 
     function startWindowDrag(event) {
+        event.preventDefault();
+        dragState = DragState.WINDOW_POSITION;
         dragEl = this;
         this.classList.add("sa-dragging");
         let boundingRect = this.getBoundingClientRect();
@@ -73,18 +88,76 @@ export async function makeWindow(tabs) {
         // We don't care where the mouse is *released*.
         if (dragEl) dragEl.classList.remove("sa-dragging");
         dragOffset = null;
+        dragState = null;
+        windowResizeRectBuffer = null;
     });
 
     document.addEventListener("mousemove", function (event) {
-        if (!dragOffset) return;
-        windowContainer.style.left = `${event.clientX - dragOffset[0]}px`;
-        windowContainer.style.top = `${event.clientY - dragOffset[1]}px`;
+        if (dragState !== null) {
+            event.preventDefault();
+        }
+
+        if (dragState === DragState.WINDOW_POSITION) {
+            windowContainer.style.left = `${event.clientX - dragOffset[0]}px`;
+            windowContainer.style.top = `${event.clientY - dragOffset[1]}px`;
+        } else if (dragState === DragState.RESIZE_BOTTOM_RIGHT) {
+            let rect = windowContainer.getBoundingClientRect();
+            let width = Math.max(event.clientX - rect.x, WINDOW_MIN_WIDTH);
+            let height = Math.max(event.clientY - rect.y, WINDOW_MIN_HEIGHT);
+            windowContainer.style.width = `${width}px`;
+            windowContainer.style.height = `${height}px`;
+        } else if (dragState === DragState.RESIZE_BOTTOM_LEFT) {
+            let rect = windowContainer.getBoundingClientRect();
+            let rightBoundary = windowResizeRectBuffer.x + windowResizeRectBuffer.width;
+            let width = Math.max(rightBoundary - event.clientX, WINDOW_MIN_WIDTH);
+            let height = Math.max(event.clientY - rect.y, WINDOW_MIN_HEIGHT);
+            let left = Math.min(event.clientX, rightBoundary - WINDOW_MIN_WIDTH);
+            windowContainer.style.left = `${left}px`;
+            windowContainer.style.width = `${width}px`;
+            windowContainer.style.height = `${height}px`;
+        }
     });
 
     minimizeButton.addEventListener("click", function () {
         isMinimized = !isMinimized;
         minimizeButton.innerText = isMinimized ? "+" : "-";
         main.style.display = isMinimized ? "none" : "flex";
+    });
+
+    const handleCont = $e("div", windowContainer, { id: "sa-window-handle-cont" });
+
+    const resizeHandles = {
+        bottomLeft: $e(
+            "div",
+            handleCont,
+            {
+                classes: ["sa-resize-handle"],
+                "style.cursor": "nesw-resize",
+                "style.left": "0px",
+                "style.bottom": "0px"
+            }
+        ),
+        bottomRight: $e(
+            "div",
+            handleCont,
+            {
+                classes: ["sa-resize-handle"],
+                "style.cursor": "nwse-resize",
+                "style.right": "0px",
+                "style.bottom": "0px"
+            }
+        )
+    };
+
+    resizeHandles.bottomLeft.addEventListener("mousedown", function (event) {
+        event.preventDefault();
+        dragState = DragState.RESIZE_BOTTOM_LEFT;
+        windowResizeRectBuffer = windowContainer.getBoundingClientRect();
+    });
+    resizeHandles.bottomRight.addEventListener("mousedown", function (event) {
+        event.preventDefault();
+        dragState = DragState.RESIZE_BOTTOM_RIGHT;
+        // windowResizeRectBuffer = windowContainer.getBoundingClientRect();
     });
 
     // Tabs
