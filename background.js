3da6781b513bf12221495432927915d3ba296d9f
somebody <onesomegit@outlook.com>
2023-06-14 11:39:23 -0500

    Indent and toggle

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 browser.browserAction.onClicked.addListener(async function () {
-  tabs = await browser.tabs.query({
-    currentWindow: true,
-    active: true,
-  });
+    tabs = await browser.tabs.query({
+        currentWindow: true,
+        active: true,
+    });
 
-  for (const tab of tabs) {
-    browser.tabs.sendMessage(tab.id, "init");
-  }
+    for (const tab of tabs) {
+        browser.tabs.sendMessage(tab.id, "init");
+    }
 });
